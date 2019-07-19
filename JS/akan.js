3a1c9058a183cb5f7b324a4b21b51e41819731b0
Imaraika <inange2013@gmail.com>
2019-07-19 12:42:21 -0700

    heading has been added

--- JS/akan.js
+++ JS/akan.js
@@ -13,11 +13,9 @@ function akanFunction(){
  var CC =  (YY-1)/100+1;
  var dayOFTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
  
+ if ( arrayAkanMale[Math.round(dayOFTheWeek)]){
  
-
- if ( (arrayAkanMale[Math.round(dayOFTheWeek)]) && (gender == "Male")){
-  console.log(arrayDay[Math.round(dayOFTheWeek)]);
-  document.getElementById("day").innerHTML =arrayDay[Math.round(dayOFTheWeek)];
-  document.getElementById("akaname").innerHTML = arrayAkanMale[Math.round(dayOFTheWeek)];
+  document.getElementById("day").innerHTML = "you were bon on :" + arrayDay[Math.round(dayOFTheWeek)];
+  document.getElementById("msg2").innerHTML = "And Your Akan Name is :" +document.getElementById("akaname").arrayAkanMale[Math.round(dayOFTheWeek)];
  }
 }
