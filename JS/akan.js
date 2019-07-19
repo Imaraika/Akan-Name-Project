a2941d355c1923bf4eb413597560130658d0fb7b
Imaraika <inange2013@gmail.com>
2019-07-19 11:47:48 -0700

    the day a pereson were born has been calculated and displayed

--- JS/akan.js
+++ JS/akan.js
@@ -1,14 +1,23 @@
 function akanFunction(){
- var name ;
- var arrayAkan = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','saturday'];
- var gender;
+ var name = "kwasi" ;
+ var gender = document.getElementById("gend").value;
+ var arrayDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','saturday'];
+ var arrayAkanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+ var arrayAkanFemale = ['Akosua','Adwoa','Abenaa','Akau','Yaa','Afua','Ama'];
+//   var arrayAkan = [0,1,2,3,4,5,6];
+// var male = document.getElementById("m").value;
+//  var female = document.getElementById("f").value;
  var DD =parseFloat(document.getElementById("dd").value);
  var MM =parseFloat(document.getElementById("mm").value);
  var YY =parseFloat(document.getElementById("yy").value);
  var CC =  (YY-1)/100+1;
  var dayOFTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
- document.getElementById("demo").innerHTML =Math.round(dayOFTheWeek);
- if (dayOFTheWeek == arrayAkan[0] && gender == "female"){
-    console.log('kwasi')
-}
+ 
+ 
+
+ if ( (arrayAkanMale[Math.round(dayOFTheWeek)]) && (gender == "Male")){
+  console.log(arrayDay[Math.round(dayOFTheWeek)]);
+  document.getElementById("day").innerHTML =arrayDay[Math.round(dayOFTheWeek)];
+  document.getElementById("akaname").innerHTML = arrayAkanMale[Math.round(dayOFTheWeek)];
+ }
 }
