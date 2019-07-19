2066c224b970b135b9e03c3ab95117dff6d7af4f
Imaraika <inange2013@gmail.com>
2019-07-19 15:51:05 -0700

    lines of codes for displaying the Akan name and the day have been added in js file

--- JS/akan.js
+++ JS/akan.js
@@ -1,24 +1,29 @@
 function akanFunction(){
  var name = "kwasi" ;
- var selection = document.getElementById('selector').value;
+ var gender = document.getElementById('selector').value;
  var arrayDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','saturday'];
  var arrayAkanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
  var arrayAkanFemale = ['Akosua','Adwoa','Abenaa','Akau','Yaa','Afua','Ama'];
-//   var arrayAkan = [0,1,2,3,4,5,6];
-// var male = document.getElementById("m").value;
-//  var female = document.getElementById("f").value;
+  var arrayAkanRes= [0,1,2,3,4,5,6];
  var DD =parseFloat(document.getElementById("dd").value);
  var MM =parseFloat(document.getElementById("mm").value);
  var YY =parseFloat(document.getElementById("yy").value);
  var CC =  (YY-1)/100+1;
  var dayOFTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-
- if ( selection == "male") {
+  if ( Math.round(dayOFTheWeek)== arrayAkanRes[Math.round(dayOFTheWeek)] && gender == "male" ){
     console.log(arrayDay[Math.round(dayOFTheWeek)]);
     document.getElementById('day').innerHTML = arrayDay[Math.round(dayOFTheWeek)];
     document.getElementById('akaname').innerHTML = arrayAkanMale[Math.round(dayOFTheWeek)];
-
 }
-    // else if(Math.round(dayOFTheWeek)==1){
-//  }
- }
+
+    else if(Math.round(dayOFTheWeek)== arrayAkanRes[Math.round(dayOFTheWeek)] && gender == "female" ){
+        console.log(arrayDay[Math.round(dayOFTheWeek)]);
+    document.getElementById('day').innerHTML = arrayDay[Math.round(dayOFTheWeek)];
+    document.getElementById('akaname').innerHTML = arrayAkanFemale[Math.round(dayOFTheWeek)];
+}
+else 
+{
+    document.getElementById("day").innerHTML = "Please Enter the valid information"
+}
+  
+}
