210480645d13b50e3da923acda5282ebaf00c200
Imaraika <inange2013@gmail.com>
2019-07-18 21:51:18 -0700

    the paragraph and function in akan JS file has been created

---
+++ JS/akan.js
@@ -0,0 +1,10 @@
+function akanFunction(){
+ var name ;
+ var gender;
+ var DD =parseFloat(document.getElementById("dob").value);
+ var MM =parseFloat(document.getElementsById("dob").value);
+ var YY =parseFloat(document.getElementById("dob").value);
+ var CC =  (YY-1)/100+1;
+ var dayOFTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+ document.getElementById("demo").innerHTML = dayOFTheWeek;
+}
